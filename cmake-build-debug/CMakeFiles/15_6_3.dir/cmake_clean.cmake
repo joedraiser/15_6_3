b57c3ef6beffc30c6397d9e37e763d3241c9c249
Evgenii.Korchagov <joedraiser@gmail.com>
2021-10-04 10:04:03 +0300

    Initial 15_6_3

---
+++ cmake-build-debug/CMakeFiles/15_6_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "15_6_3.exe"
+  "15_6_3.exe.manifest"
+  "15_6_3.pdb"
+  "CMakeFiles/15_6_3.dir/main.cpp.obj"
+  "lib15_6_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15_6_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
