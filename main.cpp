18bf9278458ef284f25d94dd34dff1fbfaa90a42
Evgenii.Korchagov <joedraiser@gmail.com>
2021-10-04 10:54:46 +0300

    Initial 15_6_3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void arrange(std::vector<int> * array)
+{
+    int temp;
+
+    for(int i=0;i<array->size();i++)
+        for(int j=0;j<array->size()-i-1;j++)
+        {
+            if((*array)[j]>(*array)[j+1])
+            {
+                temp=(*array)[j];
+                (*array)[j]=(*array)[j+1];
+                (*array)[j+1]=temp;
+            }
+        }
+}
+
+int main()
+{
+    int input=1;
+    std::vector<int> array;
+
+    do
+    {
+        std::cout << "Input: ";
+        while(input != (int)'\n')
+        {
+            std::cin >> input;
+            if (input == -1)
+            {
+                arrange(&array);
+                std::cout << "Output: " <<  array[4] << std::endl;
+                break;
+            }
+            else if (input == -2)
+                break;
+            else
+                array.push_back(input);
+        }
+    }
+    while(input!=-2);
+
     return 0;
 }
